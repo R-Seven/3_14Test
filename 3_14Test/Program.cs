b062e4c45d6d048c95adb692ff0bb4252074fdc4
宗畦 黃 <hsi040@hotmail.com>
2018-03-14 11:24:00 +0800

    加入專案檔案。

---
+++ 3_14Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_14Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*DateTime parsed;
+            if (DateTime.TryParseExact("20110119", "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsed))
+            {
+                Console.WriteLine((parsed.ToString("yyyy-MM-dd")));
+            }
+            Console.ReadLine();*/
+            string sDate = "20100504";
+            DateTime NewDate = DateTime.ParseExact(sDate, "yyyyMMdd", null, System.Globalization.DateTimeStyles.AllowWhiteSpaces);
+            Console.WriteLine(NewDate.ToString("yyyy-MM-dd"));
+            Console.ReadLine();
+        }
+    }
+}
